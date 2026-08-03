9477c93ba2501e9e43ee8130ab4737896804d09f
Tolulope Daniel <tolulopedaniel101@gmail.com>
2026-08-03 10:26:36 +0100

    update

--- node_modules/@sanity/image-url/lib/index.js
+++
@@ -1,7 +0,0 @@
-import { defineDeprecated, createImageUrlBuilder } from "./_chunks-es/compat.js";
-const deprecatedcreateImageUrlBuilder = defineDeprecated(createImageUrlBuilder);
-export {
-  createImageUrlBuilder,
-  deprecatedcreateImageUrlBuilder as default
-};
-//# sourceMappingURL=index.js.map
--- node_modules/@sanity/signed-urls/src/index.ts
+++
@@ -1,3 +0,0 @@
-export {pemToEd25519Bytes, pemToEd25519Hex} from './pem'
-export {generateSignature, signUrl, urlWithSigningParams} from './signing'
-export type {SigningOptions} from './types'
